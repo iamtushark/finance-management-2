1adf9d42e28b088e922d0cdc5348e995149948e2
tushar kamboj <tushar.kamboj@binmile.com>
2024-07-15 18:52:46 +0530

    Added: Summary Card

--- src/Routes/PrivateRoutes.tsx
+++ src/Routes/PrivateRoutes.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Screens/Dashboard/Index";
 
 const PrivateRoutes: React.FC = () => {
-  return <Routes></Routes>;
+  return <Routes>
+    <Route path="/dashboard" element={<Dashboard/ >}/>
+  </Routes>;
 };
 
 export default PrivateRoutes;
