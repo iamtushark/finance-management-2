12e8c8ff4e5b67c8126b752f05a243489ac61ff1
iamtushark <tushar.kamboj@binmile.com>
2024-07-15 12:19:28 +0530

    Fixes: Garvit Sidebar (#15)

--- src/Routes/RouteManager.tsx
+++ src/Routes/RouteManager.tsx
@@ -3,14 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import PublicRoutes from "./PublicRoutes";
 import PrivateRoutes from "./PrivateRoutes";
 import { localStorageKeys } from "../dbOperations/config";
-import CommonSideBar from "../Components/Common/CommonSideBar";
 
 const RouteManager: React.FC = () => {
   const isAuthenticated = !!localStorage.getItem(localStorageKeys.user);
 
   return (
     <>
-      <CommonSideBar />
       <BrowserRouter>
         <PublicRoutes />
         {isAuthenticated && <PrivateRoutes />}
