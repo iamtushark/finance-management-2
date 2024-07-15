4f0392e4bb37679c49b6ca6d0644c68da678ee84
Nitish Rawat <nitishrawat009@gmail.com>
2024-07-15 19:55:30 +0530

    fixed transactions page with dynamic data fetching and added state initialiser for testing

--- src/features/transaction/transactionSlice.ts
+++ src/features/transaction/transactionSlice.ts
@@ -10,7 +10,13 @@ interface TransactionSliceState {
 }
 
 const initialState: TransactionSliceState = {
-  transactions: [],
+  transactions: [
+    {
+      type: 'Expense',
+      amount: 458,
+      date: new Date(),
+    },
+  ],
   status: "idle",
 };
 
