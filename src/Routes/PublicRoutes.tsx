c50717988c4b0418414aa2dbbcec1905f21c39aa
tushar kamboj <tushar.kamboj@binmile.com>
2024-07-16 14:41:48 +0530

    Improvements: Routes

--- src/Routes/PublicRoutes.tsx
+++ src/Routes/PublicRoutes.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../Screens/Login/Index";
 import SignupPage from "../Screens/Signup/Index";
-import MiniDrawer from "../Components/Common/CommonSideBar";
 
 const PublicRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/login" element={<MiniDrawer />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      {/* <Route path="/transactions" element={<Subscriptions />} /> */}
     </Routes>
   );
 };
