2b4c87e3d45024eda7331ca0dad985ee9d4c7a46
nitish-bmt <nitish.rawat@binmile.com>
2024-07-16 10:31:19 +0530

    Components/transactions (#20)
    * Transactions component
    
    * fixed transactions page with dynamic data fetching and added state initialiser for testing
    
    ---------
    
    Co-authored-by: Nitish Rawat <nitishrawat009@gmail.com>

---
+++ src/Components/Transactions/TransactionsPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import SummarySection from './SummarySection';
+import { TableSection, AddButton } from './TableSection';
+
+const Subscriptions = () => (
+  <Container sx={{
+    p: 4,
+  }}>
+    <SummarySection />
+    <TableSection />
+    <AddButton />
+  </Container>
+);
+
+export default Subscriptions;
