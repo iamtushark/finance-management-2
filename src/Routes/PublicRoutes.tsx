122d1aec06053f42cae6aff634ecf8fba59fbc9e
nitish-bmt <nitish.rawat@binmile.com>
2024-07-16 10:31:19 +0530

    Components/transactions (#20)
    * Transactions component
    
    * fixed transactions page with dynamic data fetching and added state initialiser for testing
    
    ---------
    
    Co-authored-by: Nitish Rawat <nitishrawat009@gmail.com>

--- src/Routes/PublicRoutes.tsx
+++ src/Routes/PublicRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../Screens/Login/Index";
 import SignupPage from "../Screens/Signup/Index";
 import Subscriptions from "../Components/Transactions/TransactionsPage";
+<<<<<<< HEAD
 
 const PublicRoutes: React.FC = () => {
   return (
@@ -20,12 +21,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../Screens/Login/Index";
 import SignupPage from "../Screens/Signup/Index";
+=======
+>>>>>>> 2b4c87e (Components/transactions (#20))
 
 const PublicRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/transactions" element={<Subscriptions />} />
     </Routes>
   );
 };
