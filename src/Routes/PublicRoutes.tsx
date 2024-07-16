bfb2b279243a78186ececb656e0b76a61fe8f049
Nitish Rawat <nitishrawat009@gmail.com>
2024-07-16 18:07:05 +0530

    browser router removed

--- src/Routes/PublicRoutes.tsx
+++ src/Routes/PublicRoutes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../Screens/Login/Index";
 import SignupPage from "../Screens/Signup/Index";
+<<<<<<< HEAD
 import Subscriptions from "../Components/Transactions/TransactionsPage";
 <<<<<<< HEAD
 
@@ -23,13 +24,14 @@ import LoginPage from "../Screens/Login/Index";
 import SignupPage from "../Screens/Signup/Index";
 =======
 >>>>>>> 2b4c87e (Components/transactions (#20))
+=======
+>>>>>>> 7f7c828 (browser router removed)
 
 const PublicRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      <Route path="/transactions" element={<Subscriptions />} />
     </Routes>
   );
 };
