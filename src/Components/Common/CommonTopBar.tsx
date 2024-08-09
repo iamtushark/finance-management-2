390e298f9b1105f08c521a28f5047375bd71b969
Nitish <nitish.rawat@binmile.com>
2024-08-09 18:36:33 +0530

    fixed alignment

--- src/Components/Common/CommonTopBar.tsx
+++ src/Components/Common/CommonTopBar.tsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Container, Divider, Typography, IconButton } from "@mui/material";
 import MiniDrawer from "./CommonSideBar";
 import MenuIcon from "@mui/icons-material/Menu";
+import { DateRangePicker, SingleInputDateRangeField } from "@mui/x-date-pickers-pro";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Dayjs } from "dayjs";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-const CommonTopBar: React.FC<{ title: string }> = ({ title }) => {
+interface TopBarProps{
+  title: string,
+  dateRange?: [Dayjs | null, Dayjs | null],
+  handleDateRangeChange?: (newRange: [Dayjs | null, Dayjs | null])=>void,
+};
+
+const CommonTopBar: React.FC<TopBarProps> = (props: TopBarProps) => {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
-
   const handleDrawerToggle = () => {
     setDrawerOpen(!drawerOpen);
   };
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+
 
   return (
     <>
       <Box
         sx={{
+          position: "sticky",
           bgcolor: "#111827",
           color: "white",
-          height: "8vh",
+          height: "10vh",
           display: "flex",
           alignItems: "center",
           paddingLeft: 2,
@@ -33,8 +53,20 @@ const CommonTopBar: React.FC<{ title: string }> = ({ title }) => {
           <MenuIcon />
         </IconButton>
         <Typography variant="h4" sx={{ fontWeight: 700 }}>
-          {title}
+          {props.title}
         </Typography>
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <ThemeProvider theme={darkTheme}>
+            <DateRangePicker
+              sx={{ float:"right", marginLeft: "64vw"}}
+              slots={{ field: SingleInputDateRangeField }}
+              value={props.dateRange}
+              onChange={props.handleDateRangeChange}
+            />
+          </ThemeProvider>
+
+        </LocalizationProvider>
       </Box>
       <MiniDrawer open={drawerOpen} onClose={handleDrawerToggle} />
     </>
